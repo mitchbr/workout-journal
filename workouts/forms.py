727396d9ccd417210b03ab3f29d97e40cbc8e27c
Mitchell Brown <mitchellbrown@Mitchells-MacBook-Air.local>
2025-10-30 08:51:24 -0700

    Use text-base to avoid mobile zoom

--- workouts/forms.py
+++ workouts/forms.py
@@ -5,11 +5,11 @@ from .models import Workout, WorkoutType
 class WorkoutForm(forms.ModelForm):
     workout_type = forms.ModelChoiceField(queryset=WorkoutType.objects.all().order_by("title"), empty_label="Select a Category")
     note = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'Note'}),
+        widget=forms.TextInput(attrs={'placeholder': 'Note', 'class': 'text-base'}),
         required=False
     )
     date = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date'}),
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'text-base'}),
         input_formats=['%Y-%m-%d']
     )
 
@@ -19,11 +19,11 @@ class WorkoutForm(forms.ModelForm):
         
 class WorkoutUpdateForm(forms.ModelForm):
     note = forms.CharField(
-        widget=forms.TextInput(attrs={'placeholder': 'Note'}),
+        widget=forms.TextInput(attrs={'placeholder': 'Note', 'class': 'text-base'}),
         required=False
     )
     date = forms.DateField(
-        widget=forms.DateInput(attrs={'type': 'date'}),
+        widget=forms.DateInput(attrs={'type': 'date', 'class': 'text-base'}),
         input_formats=['%Y-%m-%d']
     )
     
